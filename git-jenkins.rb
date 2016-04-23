4d6732f15e0efcef7531e51bd8f8ce08782e3ea2
Timothy <ygmpkk@gmail.com>
2016-04-23 14:32:46 +0800

    升级版本

--- git-jenkins.rb
+++ git-jenkins.rb
@@ -1,14 +1,13 @@
 class GitJenkins < Formula
   desc "Jenkins cli for git subcommand"
   homepage "https://github.com/ygmpkk/git-jenkins"
-  url "https://github.com/ygmpkk/git-jenkins/archive/1.0.0.tar.gz"
-  version "1.0.0"
+  url "https://github.com/ygmpkk/git-jenkins/archive/1.0.1.tar.gz"
+  version "1.0.1"
   sha256 ""
 
   depends_on "jsawk"
 
   def install
-    bin.install "jenkins-cli"
     bin.install "git-jenkins"
   end
 
